71eb16143d5884dbb284615265e99a154ae0d051
theophilus28 <111762754+theophilus28@users.noreply.github.com>
2024-05-16 10:30:18 -0400

    learning about variables n stuff

--- JS-Basics/firstjavascript.js
+++ JS-Basics/firstjavascript.js
@@ -1,2 +1,30 @@
 //separate file for js code
 console.log('Hellow World')
+
+let named = 'Theo';
+console.log(name);
+//cannot use reserved keywords for variable names
+
+const interestRate = 0.3;
+//interestRate = 1; <--this would cause error bc cant reassign const
+
+let aged = 21; //num, all numbers have the same number type
+let isApproved = true; //bool
+let firstName = undefined;
+let lastName = null; //used when we want to clear value of a var
+
+//js has dynamic typing, so type of var is determined at runtime, and it can change
+
+let person = {
+    name: 'Theo',
+    age: 21
+}
+
+//dot notation
+person.name = 'Theophilus';
+
+//bracket notation
+let selection = 'name';
+person[selection] = 'MD';
+
+console.log(person.name);
